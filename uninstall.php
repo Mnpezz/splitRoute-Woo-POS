c6d08610fa6e33e116e51b64a403a34eab7eef50
Mnpezz <93685835+Mnpezz@users.noreply.github.com>
2025-04-06 10:27:23 -0500

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -9,6 +9,12 @@ delete_option('nano_split_pos_api_key');
 delete_option('nano_split_pos_enable_tips');
 delete_option('nano_split_pos_tip_percentages');
 delete_option('nano_split_pos_enable_products');
+delete_option('nano_split_pos_use_woo_tax');
+delete_option('nano_split_pos_tax_class');
+delete_option('nano_split_pos_tax_address');
+delete_option('nano_split_pos_tax_rate');
+delete_option('nano_split_pos_test_mode');
+delete_option('nano_split_pos_employees');
 
 // Delete database tables
 global $wpdb;
